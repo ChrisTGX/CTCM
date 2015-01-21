ce2699111b16e284c973663340964d6818b3be4e
christian <christian@PC>
2015-01-20 23:18:51 -0430

    Todo

--- tarea2/Tarifa.py
+++ tarea2/Tarifa.py
@@ -19,18 +19,18 @@ class Tarifa(object):
         monto = 0
 
         while(diaActual <= salida.day):
-            print("En este momento horini es " + str(horini) + " y horfin es " + str(horfin) + " y monto es " + str(monto))
+            
             # Nocturno
             if (horfin < 6) or (horini >= 18) or ((horini == 18 or horfin == 6) and llegada.minute == 0):
-                print("Nocturno")
+                
                 monto = monto + self.horaNocturna
             # Diurno
             if(horini >= 6 and horfin < 18 and horini < horfin) or ((horini == 6 or horfin == 18) and llegada.minute == 0):
-                print("Diurno")
+                
                 monto = monto + self.horaDiurna
             # Hora mas cara
             if (horini == 17 or horini == 5) and llegada.minute != 0:
-                print("Mas cara")
+                
                 monto = monto + max(self.horaDiurna, self.horaNocturna)
 
 
